7e3a5c48546c04949723adef0c41bed66ddc6e54
resident7709 <resident7709@gmail.com>
2024-03-10 12:05:30 +0300

    Add MySQL database, Prisma

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import prisma from '@/prisma/db';
 
-const Tickets = () => {
+const Tickets = async () => {
+  const tickets = await prisma.ticket.findMany();
+
+  console.log(tickets);
+
   return <div>Tickets</div>;
 };
 
