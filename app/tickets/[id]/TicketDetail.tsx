0521fb7eb136e95aac233ec310abe5528cc883f9
resident7709 <resident7709@gmail.com>
2024-03-12 22:46:39 +0300

    Setup Ticket details

---
+++ app/tickets/[id]/TicketDetail.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Ticket } from '@prisma/client';
+
+interface Props {
+  ticket: Ticket;
+}
+
+const TicketDetail = ({ ticket }: Props) => {
+  return (
+    <div>
+      <p>{ticket.title}</p>
+      <p>{ticket.description}</p>
+    </div>
+  );
+};
+
+export default TicketDetail;
