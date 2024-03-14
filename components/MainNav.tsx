e4cdfb972d8a0ea6431276d8b2a6631b1ebfceb5
resident7709 <resident7709@gmail.com>
2024-03-14 20:39:39 +0300

    Add Navigation login

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import Link from 'next/link';
+import { getServerSession } from 'next-auth';
 
 import ToggleMode from './ToggleMode';
+import MainNavLinks from './MainNavLinks';
+import options from '@/app/api/auth/[...nextauth]/options';
 
-const MainNav = () => {
+const MainNav = async () => {
+  const session = await getServerSession(options);
+
+  console.log(session);
   return (
     <div className='flex justify-between'>
+      <MainNavLinks />
       <div className='flex items-center gap-2'>
-        <Link href='/'>Dashboard</Link>
-        <Link href='/tickets'>Tickets</Link>
-        <Link href='/users'>Users</Link>
-      </div>
-      <div className='flex items-center gap-2'>
-        <Link href='/'>Logout</Link>
+        {session ? (
+          <Link href='/api/auth/signout?callbackUrl=/'>Logout</Link>
+        ) : (
+          <Link href='/api/auth/signin'>Login</Link>
+        )}
         <ToggleMode />
       </div>
     </div>
