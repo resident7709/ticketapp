e08c689516cd65842142fa0b7131c0f52e260c3f
resident7709 <resident7709@gmail.com>
2024-03-14 16:05:37 +0300

    Update User table

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import prisma from '@/prisma/db';
 
 import UserForm from '@/components/UserForm';
+import DataTableSimple from './data-table-simple';
 
-const Users = () => {
+const Users = async () => {
+  const users = await prisma.user.findMany();
+
   return (
     <div>
       <UserForm />
+      <DataTableSimple users={users} />
     </div>
   );
 };
